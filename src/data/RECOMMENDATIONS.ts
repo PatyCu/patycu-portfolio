fc0745e19c669a651c17cdadfb90130001e9f4ac
Paty Cuenca <patycuenca@gmail.com>
2024-02-06 17:36:29 +0100

    recommendations section (missing data)

---
+++ src/data/RECOMMENDATIONS.ts
@@ -0,0 +1,17 @@
+export const RECOMMENDATIONS = [
+    {
+        name: "Nina Repetto",
+        position: "Senior Software Engineer",
+        experience: "Senior Engineering Manager @ Mural",
+        location: "Remote",
+        date: "February 2024",
+        recommendation: [
+            "I had the opportunity of having Paty as my Engineer Manager for over a year, and it was a great experience. She kept the team on track towards our goals, while maintaining open communication to strike a balance between the technical and product aspects.",
+            "She created an environment of trust and collaboration, where everyone's voice was heard and where everyone would feel valued. Her genuine concern for her team members goes beyond professional aspects.",
+            "Throughout my time working with Paty, she provided constructive feedback that contributed to my professional development.",
+            "I am truly grateful to her and I highly recommend her as a manager."
+        ],
+
+        tags: ["Leadership", "Mentorship", "Product", "Technology"]
+    }
+]
