0b255bc0388113469e5e477390d339f1be3ef795
Paty Cuenca <patycuenca@gmail.com>
2024-02-07 12:52:13 +0100

    added Recommendations content and small styling fix

--- src/data/RECOMMENDATIONS.ts
+++ src/data/RECOMMENDATIONS.ts
@@ -11,5 +11,59 @@ export const RECOMMENDATIONS = [
             "Throughout my time working with Paty, she provided constructive feedback that contributed to my professional development.",
             "I am truly grateful to her and I highly recommend her as a manager."
         ]
+    },
+    {
+        name: "Nadia Romano",
+        position: "Principal Product Manager",
+        experience: "Senior Engineering Manager @ Mural",
+        location: "Remote",
+        date: "February 2024",
+        recommendation: [
+            "While working with the canvas core team, Paty was able to instill a profound sense of ownership and accountability within each member. She created an environment and the necessary methods for the team to feel empowered and take ownership of their tasks, fostering a culture of responsibility that significantly contributed to the success of the initiatives. ",
+            "Paty also played a significant role in equalizing participation and bridging knowledge gaps among the engineering team. Her effective communication skills helped ensure that everyone, regardless of experience or expertise, felt included, empowered and valued. For example, she introduced the feature captain role, which gave less senior members of the team, the opportunity to grow in their career. This contributed significantly to our team's adaptability and overall success.",
+            "Another quality that makes her stand out from the rest of the engineering managers I've worked with was her ability to identify skills that needed improvement and her dedication to creating a clear path for individual growth. She took the time to understand the strengths and weaknesses of each team member. There was no single member that did not grow under her guidance.",
+            "Paty also has a clear understanding of how to introduce changes gradually yet effectively. Her strategic approach to implementing new processes and methodologies ensured a smooth transition for the team. Through these changes, she streamlined our workflows, leading to a significant reduction in meeting times while elevating the overall level of ownership and efficiency of the team."
+        ]
+    },
+    {
+        name: "Jeffrey C. Lu",
+        position: "Senior Software Engineer",
+        experience: "Senior Engineering Manager @ Mural",
+        location: "Remote",
+        date: "February 2024",
+        recommendation: [
+            "When I joined Paty's team, I quickly discovered that it had a track reputation of being one of the highest performing teams at Mural. It was no surprise that this was due in no small part to her leadership and guidance. Everyone on the team - not just the engineers - worked well and had a high degree of trust for each other. That was the best indication to me that I had joined the right team, and more importantly, the right manager.",
+            "In my direct experience, Paty is not only a highly effective manager herself, but where she really excels is in mentoring other managers (and aspiring managers such as myself). I learned a tremendous amount about management and leadership under Paty's guidance and by following her example. The quality that I admire most is her dedication to providing feedback regularly and consistently."
+        ]
+    },
+    {
+        name: "Hernan Garchtrom",
+        position: "Senior Software Engineer",
+        experience: "Senior Engineering Manager @ Mural",
+        location: "Remote",
+        date: "January 2024",
+        recommendation: [
+            "I worked with Paty for a little over a year and over that time she has been one of the greatest managers I've ever had. She knows how to get her team to trust her so when she has to give any type of feedback it it really reaches whoever is on the other side. She's great at helping her team grow and the most important thing is that she cares about the people and about the team's goals and does magic to keep them in place and achievable."
+        ]
+    },
+    {
+        name: "Javier García Cotado",
+        position: "Principal Software Engineer",
+        experience: "Engineering Manager @ Altran",
+        location: "Barcelona, Spain",
+        date: "February 2015",
+        recommendation: [
+            "I've been working with Patricia for over a year and I've been quite impressed by her efficiency and diligence in carrying things out.For me, it was a pleasure to have someone you can discuss technical issues with, and project management issues as well. She always focuses hard to guarantee that both the project and the team succeed in their objectives. She is energetic, reliable, highly organised, trust-worthy ... An excellent manager for any company! I would not hesitate to work with her again."
+        ]
+    },
+    {
+        name: "Jordi Martínez Llamas",
+        position: "Sofrware Enineer",
+        experience: "Engineering Manager @ Altran",
+        location: "Barcelona, Spain",
+        date: "October 2014",
+        recommendation: [
+            "Patricia is the kind of team leader that every IT worker would want to work for. She has great abilities and techniques in team management, and the feature that I most value from her is the empathy with every team member, no matter the role they have, in part by her own experiences while moving on at Altran's career path that led her to the team leader role. I'm very proud of have been able to work with her and I would greatly recommend her!."
+        ]
     }
 ]
