eac2607405166e2c056ba843843e453972e6cd8e
Paty Cuenca <patycuenca@gmail.com>
2025-06-13 18:36:42 +0200

    added side projects section

--- src/data/PROJECTS.ts
+++ src/data/PROJECTS.ts
@@ -1,17 +1,78 @@
 export const PROJECTS = [
     {
-        title: "Oriol i Paty's Wedding",
-        description:
-            "The frontend for my own wedding's website and wedding registry, built with React and deployed with Netlify.",
-        link: "https://oriolipaty.com",
-        github: "https://github.com/PatyCu/wedding",
-        image: "/projects/oriolipaty_screenshot.jpg"
+        title: "Mural: Canvas Core",
+        description: "Mural is a digital workspace for visual collaboration.",
+        link: "https://mural.co",
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["React", "TypeScript", "Cypress"]
     },
     {
-        title: "this.portfolio",
-        description: "This portfolio, built with Astro, some Typescript and deployed with Netlify.",
-        link: "https://patycuenca.com",
-        github: "https://github.com/PatyCu/patycu-portfolio",
-        image: "/projects/patycuenca_screenshot.jpg"
+        title: "Mural: Devices",
+        description: "Mural apps for iOS, Android, Windows and Mac.",
+        link: "https://mural.co",
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["React Native", "Typescript", "Cypress"]
+    },
+    {
+        title: "New Relic: Dashboards",
+        description:
+            "Dashboards product is a key part of New Relic's offering,  it allows our customers to visualize their data in a way that is meaningful to them.",
+        link: "https://newrelic.com/products/dashboards",
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["React", "NodeJS", "TypeScript", "AWS"]
+    },
+    {
+        title: "New Relic: Data Explorer",
+        description: "Data Explorer is a product that allows our customers to query their data in a flexible way.",
+        link: "https://newrelic.com/products/data-explorer",
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["React", "NodeJS", "TypeScript", "AWS"]
+    },
+    {
+        title: "New Relic: Query Builder",
+        description: "Query Builder is a product that allows our customers to build queries in a visual way.",
+        link: "https://newrelic.com/products/query-builder",
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["React", "NodeJS", "TypeScript", "AWS"]
+    },
+    {
+        title: "New Relic: Vizco",
+        description:
+            "Vizco is a library that allows our customers to build visualizations in a declarative way. It extends D3, and it's used by all the product teams within New Relic.",
+        link: null,
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["javascript", "d3"]
+    },
+    {
+        title: "Typeform: the Rendering Engine",
+        description:
+            "The Rendering Engine is the core of Typeform's product. It's the piece of software that renders the forms that our customers create.",
+        link: "https://www.typeform.com/",
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["javascript", "react", "vanilla js"]
+    },
+    {
+        title: "Scytl: Election Night Reporting",
+        description:
+            "Election Night Reporting is a product that allows our customers to report the results of their elections.",
+        link: "https://www.scytl.com/en/products/election-night-reporting/",
+        github: null,
+        image: "https://via.placeholder.com/300x200",
+        tags: ["javascript", "RFC", "Java"]
+    },
+    {
+        title: "SICAS",
+        description: "Sistema d'Intermediació de les Comunitats Autònomes.",
+        link: null,
+        github: null,
+        image: "/projects/soc.png",
+        tags: ["Java", "Oracle 10", "Bea Weblogic 8.1"]
     }
 ]
---
+++ src/data/SIDE_PROJECTS.ts
@@ -0,0 +1,17 @@
+export const SIDE_PROJECTS = [
+    {
+        title: "Oriol i Paty's Wedding",
+        description:
+            "The frontend for my own wedding's website and wedding registry, built with React and deployed with Netlify.",
+        link: "https://oriolipaty.com",
+        github: "https://github.com/PatyCu/wedding",
+        image: "/projects/oriolipaty_screenshot.jpg"
+    },
+    {
+        title: "this.portfolio",
+        description: "This portfolio, built with Astro, some Typescript and deployed with Netlify.",
+        link: "https://patycuenca.com",
+        github: "https://github.com/PatyCu/patycu-portfolio",
+        image: "/projects/patycuenca_screenshot.jpg"
+    }
+]
